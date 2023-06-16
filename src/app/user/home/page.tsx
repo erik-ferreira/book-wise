8d6dec62841a033232c9b2f232a88f359b571e02
erik-ferreira <erikdesouzagf@gmail.com>
2023-06-16 00:04:03 -0300

    feat: component CardBook

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -1,9 +1,8 @@
 import { LineChart } from "lucide-react"
 
 import { Header } from "@/components/Header"
-import { Avatar } from "@/components/Avatar"
+import { CardBook } from "@/components/CardBook"
 import { TitleSection } from "@/components/TitleSection"
-import { Rating } from "@/components/Rating"
 
 export default function Home() {
   return (
@@ -11,22 +10,7 @@ export default function Home() {
       <Header label="Início" icon={LineChart} />
 
       <div className="flex gap-16">
-        <section className="w-[608px]">
-          <TitleSection label="Avaliações mais recentes" />
-
-          <article className="bg-gray-700 w-full h-[280px] rounded-md p-6">
-            <header className="flex justify-between">
-              <Avatar
-                variant="large"
-                date="Hoje"
-                username="Erik Ferreira"
-                src="https://github.com/erik-ferreira.png"
-              />
-
-              <Rating totalStar={4} />
-            </header>
-          </article>
-        </section>
+        <CardBook title="Avaliações mais recentes" />
 
         <section className="w-[324px]">
           <TitleSection label="Livros populares" href="/all" />

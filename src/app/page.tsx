53a7ac419df27e6e8788e631fd3ff0f550a2ea97
erik-ferreira <erikdesouzagf@gmail.com>
2023-06-23 11:53:20 -0300

    style: responsive in page main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 
 import { ButtonSignIn } from "@/components/ButtonSignIn"
 
+import logoPng from "../assets/logo-large.png"
 import backgroundImage from "../assets/background.png"
 
 export default function Login() {
@@ -13,10 +14,22 @@ export default function Login() {
         width={598}
         height={912}
         priority
-        className="max-w-[38%] w-full h-full"
+        className="max-w-[40%] w-full h-full object-cover rounded-[10px]
+        max-xl:max-w-[45%] max-md:hidden"
       />
 
-      <div className="mx-auto w-[372px]">
+      <div
+        className="max-w-[372px] w-full mx-auto
+        max-xl:max-w-[300px] max-lg:max-w-[250px] max-md:max-w-[372px]"
+      >
+        <Image
+          src={logoPng}
+          alt="Book Wise"
+          width={232}
+          height={58}
+          className="hidden max-md:block max-md:mb-8"
+        />
+
         <h1 className="text-2xl leading-short font-bold">Boas vindas!</h1>
         <p className="text-md leading-base text-gray-200">
           Faça seu login ou acesse como visitante.

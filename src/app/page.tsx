8407ca45b388e17f47290ace91290bb7ce5878bb
erik-ferreira <erikdesouzagf@gmail.com>
2023-07-04 23:47:08 -0300

    init adapter; update table user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image"
 
+import { getServerSession } from "next-auth"
+import { authOptions } from "./api/auth/[...nextauth]/route"
+
 import { ButtonSignIn } from "@/components/ButtonSignIn"
 
 import logoPng from "../assets/logo-large.png"
 import backgroundImage from "../assets/background.png"
 
-export default function Login() {
+export default async function Login() {
+  getServerSession(authOptions)
+
   return (
     <main className="flex items-center p-5 h-screen">
       <Image

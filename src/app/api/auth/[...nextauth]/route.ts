3f405717665a88679098e4ee34aa894366561b5a
erik-ferreira <erikdesouzagf@gmail.com>
2023-07-05 00:13:51 -0300

    feat: complete config next auth with adapter prisma

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,31 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import GitHubProvider from "next-auth/providers/github"
+import { NextApiRequest, NextApiResponse, NextPageContext } from "next"
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
-    }),
-  ],
+import { PrismaAdapter } from "@/lib/prisma-adapter"
+
+export function buildNextAuthOptions(
+  req: NextApiRequest | NextPageContext["req"],
+  res: NextApiResponse | NextPageContext["res"]
+): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      }),
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID ?? "",
+        clientSecret: process.env.GITHUB_SECRET ?? "",
+      }),
+    ],
+  }
 }
 
-const handler = NextAuth(authOptions)
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  return NextAuth(req, res, buildNextAuthOptions(req, res))
+}
 
 export { handler as GET, handler as POST }

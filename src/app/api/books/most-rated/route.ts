d4c24560e0044ce0df90aef2cc7063d03dc1d8e6
erik-ferreira <erikdesouzagf@gmail.com>
2023-07-11 18:34:53 -0300

    feat: create route to load books most rated

---
+++ src/app/api/books/most-rated/route.ts
@@ -0,0 +1,41 @@
+import { prisma } from "@/lib/prisma"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(req: NextRequest) {
+  const books = await prisma.book.findMany({
+    take: 4,
+    include: { ratings: true },
+    orderBy: {
+      ratings: {
+        _count: "desc",
+      },
+    },
+  })
+
+  const booksMostRated = books
+    .map((book) => {
+      const amountRatings = book.ratings?.length
+      const totalStarOnRating = book.ratings.reduce(
+        (sum, rating) => sum + rating.rate,
+        0
+      )
+      const ratingAverage =
+        amountRatings > 0 ? Math.floor(totalStarOnRating / amountRatings) : 0
+
+      const newBook = {
+        id: book.id,
+        name: book.name,
+        author: book.author,
+        summary: book.summary,
+        cover_url: book.cover_url,
+        total_pages: book.total_pages,
+        created_at: book.created_at,
+        ratingAverage,
+      }
+
+      return newBook
+    })
+    .sort((a, b) => b.ratingAverage - a.ratingAverage)
+
+  return NextResponse.json({ booksMostRated })
+}

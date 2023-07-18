77b4d30db58028dcf2531909dd9e9d65779fa706
erik-ferreira <erikdesouzagf@gmail.com>
2023-07-18 15:18:23 -0300

    fix: complete route to load profile data

--- src/app/api/profile/[user_id]/route.ts
+++ src/app/api/profile/[user_id]/route.ts
@@ -1,6 +1,10 @@
 import { prisma } from "@/lib/prisma"
 import { NextRequest, NextResponse } from "next/server"
 
+interface CategoryReadProps {
+  [key: string]: number
+}
+
 export async function GET(
   req: NextRequest,
   { params }: { params: { user_id: string } }
@@ -48,11 +52,32 @@ export async function GET(
     (sum, rating) => sum + rating.book.total_pages,
     0
   )
+
   const totalRatedBooks = user.ratings.length
+
   const allAuthors = user.ratings.map((rating) => rating.book.author)
   const totalAuthorsRead = allAuthors.filter(
     (author, index) => allAuthors.indexOf(author) === index
   ).length
+
+  // to calculate most read category
+  const categories = user.ratings.flatMap((rating) => {
+    return rating.book.categories.map((category) => category.category.name)
+  })
+  const categoriesRead = categories.reduce((acc, category) => {
+    acc[category] = (acc[category] || 0) + 1
+
+    return acc
+  }, {} as CategoryReadProps)
+
+  let mostReadCategory = ""
+  let maxValue = 0
+  for (const prop in categoriesRead) {
+    if (categoriesRead[prop] > maxValue) {
+      maxValue = categoriesRead[prop]
+      mostReadCategory = prop
+    }
+  }
 
   return NextResponse.json({ user })
 }

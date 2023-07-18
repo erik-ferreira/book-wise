cf269771992baffe8946b12780d2c2871d68af5b
erik-ferreira <erikdesouzagf@gmail.com>
2023-07-18 16:26:41 -0300

    add filter by query params in route user ratings

--- src/app/api/profile/[user_id]/ratings/route.ts
+++ src/app/api/profile/[user_id]/ratings/route.ts
@@ -7,6 +7,8 @@ export async function GET(
   req: NextRequest,
   { params }: { params: { user_id: string } }
 ) {
+  const queryParams = req.nextUrl.searchParams
+  const bookOrAuthorSearch = queryParams.get("bookOrAuthor") || ""
   const { user_id } = params
 
   if (!user_id) {
@@ -38,19 +40,25 @@ export async function GET(
     )
   }
 
-  const userRatings: UserRatingProps[] = user.ratings.map((rating) => {
-    return {
-      id: rating.id,
-      rate: rating.rate,
-      description: rating.description,
-      created_at: rating.created_at.toString(),
-      book: {
-        name: rating.book.name,
-        author: rating.book.author,
-        cover_url: rating.book.cover_url,
-      },
-    }
-  })
+  const userRatings: UserRatingProps[] = user.ratings
+    .map((rating) => {
+      return {
+        id: rating.id,
+        rate: rating.rate,
+        description: rating.description,
+        created_at: rating.created_at.toString(),
+        book: {
+          name: rating.book.name,
+          author: rating.book.author,
+          cover_url: rating.book.cover_url,
+        },
+      }
+    })
+    .filter(
+      (rating) =>
+        rating.book.name.includes(bookOrAuthorSearch) ||
+        rating.book.author.includes(bookOrAuthorSearch)
+    )
 
   return NextResponse.json({ userRatings })
 }

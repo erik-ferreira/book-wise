60950ced41e4b9667d3dd452518570b3f77efb1a
erik-ferreira <erikdesouzagf@gmail.com>
2023-07-19 13:45:58 -0300

    feat: add react-hook-form in create rating

--- src/components/Books/PopularBookCard/FormEvaluate.tsx
+++ src/components/Books/PopularBookCard/FormEvaluate.tsx
@@ -1,6 +1,9 @@
+import { z } from "zod"
 import { useState } from "react"
+import { useForm, Controller } from "react-hook-form"
 import { useSession } from "next-auth/react"
 import * as Dialog from "@radix-ui/react-dialog"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 import { RatingStarsInput } from "../../RatingStars/Input"
 import { Profile } from "../../Profile"
@@ -9,13 +12,44 @@ import { TitleSection } from "../../TitleSection"
 import { ButtonAction } from "../../ButtonAction"
 import { DialogPortalSignIn } from "./DialogPortalSignIn"
 
+const evaluateFormSchema = z.object({
+  rate: z.number().int().min(1).max(5),
+  description: z
+    .string()
+    .min(1, { message: "Preencha o campo para realizar a avaliação" }),
+})
+
+type EvaluateFormData = z.infer<typeof evaluateFormSchema>
+
 export function FormEvaluate() {
   const session = useSession()
   const isSigned = session.status === "authenticated"
   const ButtonEvaluate = isSigned ? "button" : Dialog.Trigger
 
-  const [valueRating, setValueRating] = useState(1)
   const [showFormEvaluate, setShowFormEvaluate] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    clearErrors,
+    control,
+    formState: { errors },
+  } = useForm<EvaluateFormData>({
+    resolver: zodResolver(evaluateFormSchema),
+    defaultValues: {
+      rate: 1,
+    },
+  })
+
+  async function handleRatingBook(data: EvaluateFormData) {
+    console.log(data)
+  }
+
+  function onChangeInput(value: string) {
+    if (value === "") {
+      // handleRatingBook({ search: "" })
+    }
+  }
 
   function handleOpenFormEvaluate() {
     if (isSigned) {
@@ -42,13 +76,32 @@ export function FormEvaluate() {
       />
 
       {showFormEvaluate && (
-        <form className="w-full h-fit bg-gray-700 rounded-md p-6 mb-3">
+        <form
+          onSubmit={handleSubmit(handleRatingBook)}
+          className="w-full h-fit bg-gray-700 rounded-md p-6 mb-3"
+        >
           <div className="flex items-center justify-between max-[375px]:flex-col max-[375px]:gap-5">
             <Profile username="Erik Ferreira" usernameIsBold />
-            <RatingStarsInput stars={valueRating} onStars={setValueRating} />
+            <Controller
+              name="rate"
+              control={control}
+              render={({ field }) => (
+                <RatingStarsInput
+                  stars={field.value}
+                  onStars={field.onChange}
+                />
+              )}
+            />
           </div>
 
-          <TextArea placeholder="Escreva sua avaliação" />
+          <TextArea
+            placeholder="Escreva sua avaliação"
+            error={errors?.description?.message}
+            {...register("description", {
+              onBlur: () => clearErrors(),
+              onChange: (e) => onChangeInput(e.target.value),
+            })}
+          />
 
           <div className="flex gap-2 justify-end">
             <ButtonAction
@@ -56,7 +109,7 @@ export function FormEvaluate() {
               icon="cancel"
               onClick={handleCloseFormEvaluate}
             />
-            <ButtonAction type="button" />
+            <ButtonAction type="submit" />
           </div>
         </form>
       )}
--- src/components/Form/TextArea.tsx
+++ src/components/Form/TextArea.tsx
@@ -1,23 +1,47 @@
-import { TextareaHTMLAttributes } from "react"
+import { TextareaHTMLAttributes, forwardRef } from "react"
 
 import { twMerge } from "@/utils/tw-merge"
 
-interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
-
-export function TextArea({ className, ...rest }: TextAreaProps) {
-  return (
-    <label className="block w-full h-36 mt-6 mb-3 bg-gray-800 border border-gray-500 rounded focus-within:border-green-200 relative">
-      <textarea
-        className={twMerge(
-          "w-full h-full bg-transparent px-5 py-[0.875rem] outline-none resize-none text-sm leading-base text-gray-200",
-          className
-        )}
-        {...rest}
-      />
-
-      <span className="text-xs text-gray-400 leading-base absolute right-1.5 bottom-1">
-        0/450
-      </span>
-    </label>
-  )
+interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  error?: string
 }
+
+// eslint-disable-next-line react/display-name
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ className, error, ...rest }, ref) => {
+    return (
+      <>
+        <label
+          className={twMerge(
+            "block w-full h-36 mt-6  bg-gray-800 border border-gray-500 rounded relative",
+            {
+              "border-red-500": !!error,
+              "focus-within:border-green-200": !error,
+            }
+          )}
+        >
+          <textarea
+            ref={ref}
+            className={twMerge(
+              "w-full h-full bg-transparent px-5 py-[0.875rem] outline-none resize-none text-sm leading-base text-gray-200",
+              className
+            )}
+            {...rest}
+          />
+
+          <span className="text-xs text-gray-400 leading-base absolute right-1.5 bottom-1">
+            0/450
+          </span>
+        </label>
+
+        <span
+          className={twMerge("text-xs text-gray-300 mb-3", {
+            "text-red-500": error,
+          })}
+        >
+          {error || "Informe o que achou do livro"}
+        </span>
+      </>
+    )
+  }
+)

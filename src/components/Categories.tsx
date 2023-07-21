5c6d992d020c1060959e107f60f1ccaa736c1e8e
erik-ferreira <erikdesouzagf@gmail.com>
2023-07-21 18:38:04 -0300

    fix: change category value id to name

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -23,12 +23,12 @@ export function Categories({
       onValueChange={onChangeCategorySelected}
     >
       {categories?.map((category) => {
-        const isCategorySelected = category.id === categorySelected
+        const isCategorySelected = category.name === categorySelected
 
         return (
           <ToggleGroup.Item
             key={category.id}
-            value={category.id}
+            value={category.name}
             className={classnames(
               "py-1 px-4 rounded-full flex items-center justify-center leading-short border whitespace-nowrap transition-colors hover:text-gray-100 hover:border-purple-100 hover:bg-purple-200",
               {

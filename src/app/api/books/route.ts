945da859c280ca28dfdcf1c6041346c0d845c73c
erik-ferreira <erikdesouzagf@gmail.com>
2023-07-21 18:37:08 -0300

    add filter by category in route books

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -7,12 +7,20 @@ export async function GET(req: NextRequest) {
   const queryParams = req.nextUrl.searchParams
   const bookOrAuthorSearch = queryParams.get("bookOrAuthor") || ""
   const userId = queryParams.get("userId") || ""
+  const categoryName = queryParams.get("category") || ""
 
   const allBooks = await prisma.book.findMany({
     where: {
-      OR: [
-        { name: { contains: bookOrAuthorSearch } },
-        { author: { contains: bookOrAuthorSearch } },
+      categories: {
+        some: { category: { name: { contains: categoryName } } },
+      },
+      AND: [
+        {
+          OR: [
+            { name: { contains: bookOrAuthorSearch } },
+            { author: { contains: bookOrAuthorSearch } },
+          ],
+        },
       ],
     },
     orderBy: {
